d2c05ccf4366c2b0ff0ccd55b000de283c384907
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-04 22:11:12 -0500

    started about page

--- frontend/src/components/navbar/PHnavbar.jsx
+++ frontend/src/components/navbar/PHnavbar.jsx
@@ -2,17 +2,17 @@
 
 export default function PHnavbar() {
   return (
-    <nav class="py-8 bg-transparent mb-10">
-      <div class="md:container px-4 md:px-0 mx-auto flex justify-between">
+    <nav className="py-8 bg-transparent mb-10">
+      <div className="md:container px-4 md:px-0 mx-auto flex justify-between">
         <img
-          class="w-16"
+          className="w-16"
           src="https://capp.nicepage.com/bf483e57c95e271afd13aa25e2ecbbe017068d1d/images/default-logo.png"
           alt=""
         />
-        <div class="space-y-1.5 cursor-pointer">
-          <div class="h-1 w-8 bg-gray-600 rounded"></div>
-          <div class="h-1 w-8 bg-gray-600 rounded"></div>
-          <div class="h-1 w-8 bg-gray-600 rounded"></div>
+        <div className="space-y-1.5 cursor-pointer">
+          <div className="h-1 w-8 bg-gray-600 rounded"></div>
+          <div className="h-1 w-8 bg-gray-600 rounded"></div>
+          <div className="h-1 w-8 bg-gray-600 rounded"></div>
         </div>
       </div>
     </nav>
